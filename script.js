4eb3a8fbc2cddfccd3e98ed6bcb85e4ceddf18e2
Safarov <elkhan.safarov10@yandex.com>
2020-01-28 14:47:35 +0500

    Сохрание в

--- script.js
+++ script.js
@@ -16,20 +16,21 @@ let products = [
     {
         name: 'Black Tshirt',
         tag: 'blacktshirt',
-        price: 15,
+        price: 10,
         inCart: 0
     },
     {
         name: 'Grey Tshirt',
         tag: 'blackhoddie',
-        price: 20,
+        price: 25,
         inCart: 0
     }
 ];
 
 for(let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        cartNumbers();
+        cartNumbers(products[i]);
+        totalCost(products[i]);
     })
 }
 function onLoadCartNumbers() {
@@ -38,7 +39,7 @@ function onLoadCartNumbers() {
         document.querySelector('.cart span').textContent = productNumbers;
     }
 }
-function cartNumbers(){
+function cartNumbers(product){
     let productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
     if(productNumbers) {
@@ -47,6 +48,37 @@ function cartNumbers(){
     } else {
         localStorage.setItem('cartNumbers', 1);
         document.querySelector('.cart span').textContent = 1;
+    }
+    setItems(product);
+}
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if(cartItems != null) {
+        if(cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+}
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+    
+    if(cartCost != null){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem('totalCost', cartCost + product.price);
+    } else {
+        localStorage.setItem('totalCost', product.price);
     }
 }
 onLoadCartNumbers();
